52d9fca8f1b0f26b3b6377d6cdceafccc2854fb8
Peter Gibbs <pgibbs65@gmail.com>
2019-02-17 21:01:56 -0700

    added get echo

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(bodyParser.text({
         return 'text';
     }
 }));
-
+app.get('/get',function(req,res){
+    console.log(req.body);
+    res=res.status(200)
+    if(req.get('Content-Type')){
+        console.log("Content-Type: "+req.get('Content-Type'))
+        res=res.type(req.get('Content-Type'));
+    }
+    res.send(req.body)
+});
 app.post('/post',function(req,res){
     console.log(req.body);
     res=res.status(200)
